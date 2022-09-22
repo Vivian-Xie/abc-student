1848a298d75eb9043d6b10c287cc43f8018d7ec8
Vivian <xx2185@nyu.edu>
2022-09-22 13:14:22 +0800

    mini-pro1

---
+++ mini-projects/mini-project1/script.js
@@ -0,0 +1,8 @@
+let content=document.querySelector("#content")
+let bolder=document.querySelector("#larger")
+console.log("hah");
+bolder.onclick=function (){
+    content.style.fontSize="20px";
+    console.log("haha");
+}
+// bolder.addEventListener("click",textSize)
