246e33acc8449c671a6323981c160bd489ba29ab
Vivian <xx2185@nyu.edu>
2022-09-22 13:27:30 +0800

    changes

--- mini-projects/mini-project1/script.js
+++ mini-projects/mini-project1/script.js
@@ -1,7 +1,7 @@
 let content=document.querySelector("#content")
 let bolder=document.querySelector("#larger")
 console.log("hah");
-bolder.onclick=function (){
+content.onclick=function (){
     content.style.fontSize="20px";
     console.log("haha");
 }
