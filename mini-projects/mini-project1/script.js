ff423b85fc9567ee3c58d3d33225a26ced30c26f
Vivian <xx2185@nyu.edu>
2022-09-27 07:52:59 +0800

    coding1

--- mini-projects/mini-project1/script.js
+++ mini-projects/mini-project1/script.js
@@ -1,16 +1,25 @@
 // let content=document.querySelector("#content")
 let bolder = document.getElementById("bold")
 let virgin = document.getElementById("virgin")
-// let color=document.querySelector("#virgin")
+let color = document.querySelector("#virgin")
+let teal = document.querySelector("#TealLove")
+let img = document.createElement("image")
 bolder.style.fontSize = "30px";
 // color.style.backgroundColor="black"
 // color.style.height=Math.ceil(Math.random()*10)*10+"px";
 // console.log("hah");
-color.onclick = function () {
-    // content.style.fontSize="20px";
-    console.log("haha");
-}
 virgin.onclick = function () {
-    // content.style.fontSize="20px";
     console.log("haha");
+    virgin.innerHTML = "<img src='color.jpg'></img>"
+    virgin.firstElementChild.style.position = "relative"
+
+}
+teal.onclick = function () {
+    console.log("haha");
+    teal.innerHTML = "<img src='color.jpg'></img>"
+    teal.firstElementChild.stylewidth = "1600px"
+    teal.firstElementChild.style.position = "relative"
+    teal.firstElementChild.style.right = "0px"
+
+
 }
