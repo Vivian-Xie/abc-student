0289e596c976c02adab0f821a0889f502356db7b
Vivian <xx2185@nyu.edu>
2022-10-15 06:04:55 +0800

    projectA interaction

--- projects/projectA/script.js
+++ projects/projectA/script.js
@@ -7,20 +7,27 @@ let string=[
 "I doubt whether your IP adress is telling the truth. <br>let me see where you are.",
 "",//6 btn
 "",//7ans
-"OK. Now I will learn about the language you are using.",
+"OK. Now I want to know about the language you are using.",
 "",//9 btn
 "",//10 ans
 "The next question is how much battery do you have?",
 "",
 "",
-
+"I'm also interested in your clipboard.",
+"",
+"",
+"That's all the questions I have. <br>Here is the result for you.",
+""
 ]
 
 
 let btnContent=[
 "I agree to provide my IP address",
 "I agree to provide my geolocation",
-"I agree to provide the language for my system"
+"I agree to provide the language for my system",
+"I agree to provide my battery information",
+"I agree to provide my clipboard Content ",
+""
 
 ]
 let word=document.querySelector(".word")
@@ -39,25 +46,26 @@ var geocity,batterNum;
 window.onclick=function(){
     console.log(btnNum);
     if (check) return
-    if (count>0){
+    if (count>0&&count<19){
         if (count%3==0){
             check=true
             btn.innerHTML=btnContent[btnNum]
+            ans.innerHTML="......"
             btn.style.display="block"
         }else if(count%3==2){
             ans.style.display="none"
         }
     }
+    if (count==19) return
 word.innerHTML=string[count]
     count+=1;
-
 }
 
 btn.onclick=function(){
     check=false;
     btn.style.display="none"
     btnNum++;
-    if (btnNum>0){
+    if (btnNum>0&&btnNum<6){
         ans.style.display="block"
     }
     if (btnNum==1){
@@ -71,6 +79,9 @@ btn.onclick=function(){
         
     }else if(btnNum==4){
         getBattery();
+        
+    }else if(btnNum==5){
+        getClipboardContents();
         
     }
         
@@ -137,7 +148,7 @@ function getCity(lat, lng) {
                 // console.log(geocity);
                 ans.innerHTML = "Caught you! It seems that you are using vpn. <br>Your IP address is in "+ipaddress+", <br>but you are physically on "+geocity+"."
             } else {
-                ans.innerHTML = "Am I too suspicious? It's true that you are in" + ipaddress+" ."
+                ans.innerHTML = "Am I too suspicious? It's true that you are in" + ipaddress+"."
             }
             return;
         }
@@ -148,10 +159,18 @@ function getCity(lat, lng) {
 /////btn 3
 function getLanguage(){
     var lang = navigator.userLanguage||window.navigator.language;
-    if (lang.substring(0,2)==iplancode){
-        ans.innerHTML = "Wow, you are using the official language of the land you are staying.";
+    lang=lang.substring(0,2)
+    if (lang=='zh'){
+        ans.innerHTML="The language you use on your device is Chinese."
+    }else if(lang=='en'){
+        ans.innerHTML="The language you use on your device is English."
+
+    }
+    if (lang==iplancode){
+        // console.log(lang==iplancode)
+        ans.innerHTML += "<br>Wow, you are using the official language of the land you currently stay on.";
     }else{
-            language.innerHTML = "It seems that you are carrying another language with you on this computer. Will";
+            language.innerHTML += "<br>It seems that you are carrying another language with you on this computer.<br> Is it your mother tongue? ";
     }
 }
 
@@ -160,17 +179,40 @@ function getBattery(){
 navigator.getBattery().then(function(battery) {
     batteryNum = battery.level * 100
     if (batteryNum==100){
-        ans.innerHTML="100%? I see the energy burning actively."
-    }else if(batteryNum>50&&batteryNum<100){
-        ans.innerHTML="Medium "
+        ans.innerHTML="100%? Your energy is so full that it starts to overflow!"
+    }else if(batteryNum>50){
+        ans.innerHTML=batteryNum+"%. Dynamic energy for daily use."
+    }else if(batteryNum>20){
+        ans.innerHTML=batteryNum+"%. Save your energy. I don't think you will be able to keep energetic for a long time."
+    }else if(batteryNum<20){
+        ans.innerHTML=batteryNum+"%. Replenish your energy as quickly as possible."
     }
 
-    var charging = battery.charging ? "yes" : "no";
-    if (charging === "yes"){
-        document.getElementsByClassName('batteryTime')[0].innerHTML = 'Charging';
-    }else {
-        document.getElementsByClassName('batteryTime')[0].innerHTML = 'Not Charging';
-    }
+    // var charging = battery.charging ? "yes" : "no";
+    // if (charging === "yes"){
+    //     document.getElementsByClassName('batteryTime')[0].innerHTML = 'Charging';
+    // }else {
+    //     document.getElementsByClassName('batteryTime')[0].innerHTML = 'Not Charging';
+    // }
 })
+}
+
+
+////btn 5
+
+async function getClipboardContents() {
+    try {
+        const text = await navigator.clipboard.readText();
+        console.log('Pasted content: ', text);
+        if (text == '') {
+            ans.innerText += "It seems that you have nothing in your clipboard."
+        } else {
+            ans.innerHTML ="You are copying the line below.<br>Where are you going to paste it?<br>" +text;
+        }
+    } catch (err) {
+        console.error('Failed to read clipboard contents: ', err);
+        ans.innerText = "It seems that you have nothing in your clipboard."
+
+    }
 }
       
