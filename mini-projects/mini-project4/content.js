1b2524d28e415f6f137ab9abb7ea43c6322e7867
Vivian <xx2185@nyu.edu>
2022-10-25 13:47:26 +0800

    zip update

--- mini-projects/mini-project4/content.js
+++ mini-projects/mini-project4/content.js
@@ -5,7 +5,8 @@ let text=document.getElementsByTagName("div")
 for (let i=3;i<text.length;i+=2){
     text[i].innerHTML="Go Back to study."
     text[i].style.fontSize="large"
-    
+    text[i].style.textAlign="center"
+    // document.text[i].styleSheets[0].addClass('spin')
 }
 
 // setTimeout(() => {
