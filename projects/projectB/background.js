efded615b7eafd685c02ce2fa88d9c8ff8f90193
Vivian <xx2185@nyu.edu>
2022-11-03 10:57:13 +0800

    popup window delete

--- projects/projectB/background.js
+++ projects/projectB/background.js
@@ -1,102 +1,102 @@
-var lizardImg = chrome.runtime.getURL('about:blank');
-let lizardWidth=300;
-let lizardHeight=500;
-var titleBar = 22;
+// var lizardImg = chrome.runtime.getURL('https://vivian-xie.github.io/abc-student/projects/projectB/lizard.jpg');
+// let lizardWidth=300;
+// let lizardHeight=500;
+// var titleBar = 22;
 
-var displays = [];
-chrome.system.display.getInfo(function (ds){
-  for (var display of ds){
-    displays.push(display.bounds);
-  }
-});
-
-function bodify(win){
-  for (var display of displays){
-    if (win.left >= display.left && win.left <= display.left + display.width &&
-      win.top >= display.top && win.top <= display.top + display.height){
-
-        var update = false;
-        if (win.left - display.left < lizardWidth){
-          win.left = display.left + lizardWidth;
-          update = true;
-        }
-
-
-
-        var params = {
-          left: win.left,
-          top: win.top,
-          width: win.width,
-          height: win.height
-        };
-        
-
-        if (update){
-          chrome.windows.update(win.id, params, function (win){
-            appendliz(win);
-          });
-        } else {
-          appendliz(win);
-        }
-        break;
-    }
-  }
-}
-
-function appendliz(win){
-  var lizardTop = win.top + (win.height / 4);
-  var lizard = win.left - lizardWidth;
-  lizard.moveTo(lizardLeft, lizardTop);
-  setTimeout(checkBod, 500);
-}
-
-
-
-var currentWin;
-
-
-chrome.windows.getCurrent({}, function (win){
-  currentWin = win;
-  lizard = window.open(lizardImg, '', 'width=' + lizardWidth + ', height=' + lizardHeight);
-});
-
-var refocusing = false;
-chrome.windows.onFocusChanged.addListener(function (windowId){
-  if ( windowId != -1 && appendageIds.indexOf(windowId) == -1 && !refocusing){
-    refocusing = true;
-    for (var id of appendageIds){
-      chrome.windows.update(id, {focused: true});
-    }
-    chrome.windows.update(windowId, {focused: true}, function (){
-      refocusing = false;
-      
-      chrome.windows.get(windowId, {}, function (win){
-        currentWin = win;
-        bodify(win);
-      });
-    });
-
-  }
-});
-
-// ready = false;
-// appendageIds = [];
-// chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse){
-//   appendageIds.push(sender.tab.windowId);
-//   if (appendageIds.length >= 5){
-//     ready = true;
+// var displays = [];
+// chrome.system.display.getInfo(function (ds){
+//   for (var display of ds){
+//     displays.push(display.bounds);
 //   }
 // });
 
-function checkBod(){
-  chrome.windows.get(currentWin.id, {}, function (win){
-    if (win.left != currentWin.left || win.top != currentWin.top ||
-      win.width != currentWin.width || win.height != currentWin.height){
-        bodify(win);
-    } else {
-      setTimeout(checkBod, 500);
-    }
-  });
-}
+// function bodify(win){
+//   for (var display of displays){
+//     if (win.left >= display.left && win.left <= display.left + display.width &&
+//       win.top >= display.top && win.top <= display.top + display.height){
 
-setTimeout(checkBod, 500);
+//         var update = false;
+//         if (win.left - display.left < lizardWidth){
+//           win.left = display.left + lizardWidth;
+//           update = true;
+//         }
+
+
+
+//         var params = {
+//           left: win.left,
+//           top: win.top,
+//           width: win.width,
+//           height: win.height
+//         };
+        
+
+//         if (update){
+//           chrome.windows.update(win.id, params, function (win){
+//             appendliz(win);
+//           });
+//         } else {
+//           appendliz(win);
+//         }
+//         break;
+//     }
+//   }
+// }
+
+// function appendliz(win){
+//   var lizardTop = win.top + (win.height / 4);
+//   var lizard = win.left - lizardWidth;
+//   lizard.moveTo(lizardLeft, lizardTop);
+//   setTimeout(checkBod, 500);
+// }
+
+
+
+// var currentWin;
+
+
+// chrome.windows.getCurrent({}, function (win){
+//   currentWin = win;
+//   lizard = window.open(lizardImg, '', 'width=' + lizardWidth + ', height=' + lizardHeight);
+// });
+
+// var refocusing = false;
+// chrome.windows.onFocusChanged.addListener(function (windowId){
+//   if ( windowId != -1 && appendageIds.indexOf(windowId) == -1 && !refocusing){
+//     refocusing = true;
+//     for (var id of appendageIds){
+//       chrome.windows.update(id, {focused: true});
+//     }
+//     chrome.windows.update(windowId, {focused: true}, function (){
+//       refocusing = false;
+      
+//       chrome.windows.get(windowId, {}, function (win){
+//         currentWin = win;
+//         bodify(win);
+//       });
+//     });
+
+//   }
+// });
+
+// // ready = false;
+// // appendageIds = [];
+// // chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse){
+// //   appendageIds.push(sender.tab.windowId);
+// //   if (appendageIds.length >= 5){
+// //     ready = true;
+// //   }
+// // });
+
+// function checkBod(){
+//   chrome.windows.get(currentWin.id, {}, function (win){
+//     if (win.left != currentWin.left || win.top != currentWin.top ||
+//       win.width != currentWin.width || win.height != currentWin.height){
+//         bodify(win);
+//     } else {
+//       setTimeout(checkBod, 500);
+//     }
+//   });
+// }
+
+// setTimeout(checkBod, 500);
--- projects/projectB/content.js
+++ projects/projectB/content.js
@@ -3,20 +3,10 @@ let eatenOnes = [];
 let tongue=document.createElement('div');
 document.body.appendChild(tongue);
 tongue.className="tongue";
-
-
-function createWindow(src, width, height){
-  var win = window.open(src, "_new", "width="+width+",height="+height);
-  win.addEventListener("resize", function(){
-    console.log("Resized");
-    win.resizeTo(width, height);
-  });
-}
-  //create the lizard
-  createWindow("about:blank", 300, 400);
-
-
-
+let liz=document.createElement('div');
+document.body.appendChild(liz);
+liz.className="liz";
+let start=false;
 
 const getOffset = (el) => {
   const rect = el.getBoundingClientRect();
@@ -31,10 +21,10 @@ const getOffset = (el) => {
 for (let i = 0; i < tags.length; i++) {
   
   // find out which tags are actualy relevant (eg not empty strings)
-  if (tags[i].textContent != "") {
+  if (tags[i].textContent != ""&&tags[i].innerHTML != "") {
     tags[i].addEventListener("mouseover", (e) => {
-      
-      
+      if (start) return
+      start=true
       eatenOnes.push({
         text: tags[i].textContent,
         link: tags[i].href
@@ -43,40 +33,65 @@ for (let i = 0; i < tags.length; i++) {
       setTimeout(() => {
         tags[i].style.visibility = "hidden";
         tags[i].style.pointerEvents = "none";
-      }, 500);
+      }, 1000);
 
-      console.log("Leon's method"+getOffset(tags[i]).left+"+"+getOffset(tags[i]).top);
       // console.log(getOffset(tags[i]).left + "+" + getOffset(tags[i]).top);
       // set the initial center pos of the link the lizard will eat
       let tagX=getOffset(tags[i]).left+getOffset(tags[i]).width/2;
       let tagY=getOffset(tags[i]).top+getOffset(tags[i]).height/2;
-
+      console.log("Leon's method"+tagX+"+"+tagY);
+      
       let tongueX=0;
       // can be changed according to the lizard
       let tongueY=window.innerHeight/2;
-      let a = tagX - tongueX;
+      let a = tagX - tongueX-20;
       let b = tagY - tongueY; 
+      if (tagX==0&&tagY==0) {
+        start=false
+        return
+      }
+      console.log("after cal"+a+"+"+b);
       // keep the tongue grow and shrink according to the pos of a,b
       let tongueLength = Math.sqrt(a*a + b*b)
       // console.log("rotation angle is "+(Math.atan(tongueWidth/tongueHeight))*180/Math.PI);
       
       // rotation center and angle
-      tongue.style.backgroundImage.size="100%"
-      tongue.style.transformOrigin="0 50%"
-
-      tongue.style.width=tongueLength+"px"
-      setTimeout(()=>{
-        tongue.style.width="0px"
+      // if(a==0&&b==0){
         
-      },500)
-      // tongue.style.width=(tongxueWidth**2+tongueHeight**2)*0.5+"px";
+        tongue.style.backgroundImage.size="100%"
+        tongue.style.transformOrigin="0% 50%"
+        liz.style.backgroundImage.size="100%"
+        liz.style.transformOrigin="0% 50%"
+        liz.style.left="0px"
+        tongue.style.marginLeft="101px"
+        setTimeout(()=>{
+          tongue.style.width=tongueLength+"px"
+          liz.style.left="10px"
+          
+        },500)
+        setTimeout(()=>{
+          tongue.style.marginLeft="0px"
+          tongue.style.width="0px"
+          liz.style.left="-100px"
+          start=false
+        },1000)
+        // tongue.style.width=(tongxueWidth**2+tongueHeight**2)*0.5+"px";
+        
+        // angle between two points:  Math.atan2(p2.y - p1.y, p2.x - p1.x);
+     
 
-      // angle between two points:  Math.atan2(p2.y - p1.y, p2.x - p1.x);
-      let angle = (Math.atan2(tagY-tongueY, tagX-tongueX)) * 180 / Math.PI;
+    
+      let angle = (Math.atan2(b, a)) * 180 / Math.PI;
       // tags[i].innerHTML+=angle;
       tongue.style.transform="rotate("+angle+"deg)"
-
+      chrome.runtime.sendMessage(
+        { msg: eatenOnes }
+        );
+      // }else{
+      //   start=false
+      // }
     })
     
   }
 }
+  
