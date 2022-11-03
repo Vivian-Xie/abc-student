c63a163b4157f495d2ca7d9bc1d4d33e5e69b0b3
Vivian <xx2185@nyu.edu>
2022-11-03 13:29:24 +0800

    project B final ver

--- projects/projectB/background.js
+++ projects/projectB/background.js
@@ -1,102 +1,16 @@
-// var lizardImg = chrome.runtime.getURL('https://vivian-xie.github.io/abc-student/projects/projectB/lizard.jpg');
-// let lizardWidth=300;
-// let lizardHeight=500;
-// var titleBar = 22;
 
-// var displays = [];
-// chrome.system.display.getInfo(function (ds){
-//   for (var display of ds){
-//     displays.push(display.bounds);
-//   }
-// });
+chrome.runtime.onMessage.addListener( function(request,sender,sendResponse)
+{
+    let eaten = request.content;
+    chrome.storage.local.get({ linkArray:[] }, function(items) {
+      items.linkArray.push((eaten)); 
+      chrome.storage.local.set(items);
+  })
+})
 
-// function bodify(win){
-//   for (var display of displays){
-//     if (win.left >= display.left && win.left <= display.left + display.width &&
-//       win.top >= display.top && win.top <= display.top + display.height){
+chrome.action.onClicked.addListener((tab) => {
 
-//         var update = false;
-//         if (win.left - display.left < lizardWidth){
-//           win.left = display.left + lizardWidth;
-//           update = true;
-//         }
-
-
-
-//         var params = {
-//           left: win.left,
-//           top: win.top,
-//           width: win.width,
-//           height: win.height
-//         };
-        
-
-//         if (update){
-//           chrome.windows.update(win.id, params, function (win){
-//             appendliz(win);
-//           });
-//         } else {
-//           appendliz(win);
-//         }
-//         break;
-//     }
-//   }
-// }
-
-// function appendliz(win){
-//   var lizardTop = win.top + (win.height / 4);
-//   var lizard = win.left - lizardWidth;
-//   lizard.moveTo(lizardLeft, lizardTop);
-//   setTimeout(checkBod, 500);
-// }
-
-
-
-// var currentWin;
-
-
-// chrome.windows.getCurrent({}, function (win){
-//   currentWin = win;
-//   lizard = window.open(lizardImg, '', 'width=' + lizardWidth + ', height=' + lizardHeight);
-// });
-
-// var refocusing = false;
-// chrome.windows.onFocusChanged.addListener(function (windowId){
-//   if ( windowId != -1 && appendageIds.indexOf(windowId) == -1 && !refocusing){
-//     refocusing = true;
-//     for (var id of appendageIds){
-//       chrome.windows.update(id, {focused: true});
-//     }
-//     chrome.windows.update(windowId, {focused: true}, function (){
-//       refocusing = false;
-      
-//       chrome.windows.get(windowId, {}, function (win){
-//         currentWin = win;
-//         bodify(win);
-//       });
-//     });
-
-//   }
-// });
-
-// // ready = false;
-// // appendageIds = [];
-// // chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse){
-// //   appendageIds.push(sender.tab.windowId);
-// //   if (appendageIds.length >= 5){
-// //     ready = true;
-// //   }
-// // });
-
-// function checkBod(){
-//   chrome.windows.get(currentWin.id, {}, function (win){
-//     if (win.left != currentWin.left || win.top != currentWin.top ||
-//       win.width != currentWin.width || win.height != currentWin.height){
-//         bodify(win);
-//     } else {
-//       setTimeout(checkBod, 500);
-//     }
-//   });
-// }
-
-// setTimeout(checkBod, 500);
+  chrome.tabs.query({}, (tabs) => tabs.forEach( tab =>  
+    chrome.scripting.executeScript({target: { tabId: tab.id },files: ['content.js']}))
+  )
+});
--- projects/projectB/content.js
+++ projects/projectB/content.js
@@ -1,97 +1,103 @@
 let tags = document.getElementsByTagName("a");
-let eatenOnes = [];
+let eating = true;
 let tongue=document.createElement('div');
 document.body.appendChild(tongue);
 tongue.className="tongue";
 let liz=document.createElement('div');
 document.body.appendChild(liz);
 liz.className="liz";
+// let photo=document.createElement('img');
+// liz.appendChild(photo)
+// photo.src="https://vivian-xie.github.io/abc-student/projects/tongue2.png"
 let start=false;
 
+
+
 const getOffset = (el) => {
-  const rect = el.getBoundingClientRect();
-  return {
-    left: rect.left,
-    top: rect.top,
-    width: rect.width,
-    height: rect.height
-  };
+    const rect = el.getBoundingClientRect();
+    return {
+        left: rect.left,
+        top: rect.top,
+        width: rect.width,
+        height: rect.height
+    };
 }
+console.log(eating+"default")
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    console.log("received");
+    if (request.msg == "pause") {
+        eating = false;
+        console.log(eating+"after clicked false");
+    }else{
+        eating = true;
+        console.log(eating+"unclicked true");
+    }
+})
 
+  
+console.log(eating+"looping true");
 for (let i = 0; i < tags.length; i++) {
-  
-  // find out which tags are actualy relevant (eg not empty strings)
-  if (tags[i].textContent != ""&&tags[i].innerHTML != "") {
-    tags[i].addEventListener("mouseover", (e) => {
-      if (start) return
-      start=true
-      eatenOnes.push({
-        text: tags[i].textContent,
-        link: tags[i].href
-      });
-      // console.log(eatenOnes);
-      setTimeout(() => {
-        tags[i].style.visibility = "hidden";
-        tags[i].style.pointerEvents = "none";
-      }, 1000);
+    if (tags[i].textContent != "") {
+        tags[i].addEventListener("mouseover", () => {
+            if(eating == true){
+                if (start) return
+                start=true
+                if (tags[i].classList.contains("lizardHasEatenThis") == false) {
+                    let eatenLink = {
+                        text: tags[i].textContent,
+                        link: tags[i].href
+                    }
+                    setTimeout(()=>{
+                    tags[i].style.visibility = "hidden";
+                    tags[i].style.pointerEvents = "none";
+                    tags[i].classList.add("lizardHasEatenThis");
+                    }, 1000);
 
-      // console.log(getOffset(tags[i]).left + "+" + getOffset(tags[i]).top);
-      // set the initial center pos of the link the lizard will eat
-      let tagX=getOffset(tags[i]).left+getOffset(tags[i]).width/2;
-      let tagY=getOffset(tags[i]).top+getOffset(tags[i]).height/2;
-      console.log("Leon's method"+tagX+"+"+tagY);
-      
-      let tongueX=0;
-      // can be changed according to the lizard
-      let tongueY=window.innerHeight/2;
-      let a = tagX - tongueX-20;
-      let b = tagY - tongueY; 
-      if (tagX==0&&tagY==0) {
-        start=false
-        return
-      }
-      console.log("after cal"+a+"+"+b);
-      // keep the tongue grow and shrink according to the pos of a,b
-      let tongueLength = Math.sqrt(a*a + b*b)
-      // console.log("rotation angle is "+(Math.atan(tongueWidth/tongueHeight))*180/Math.PI);
-      
-      // rotation center and angle
-      // if(a==0&&b==0){
+                    let tagX=getOffset(tags[i]).left+getOffset(tags[i]).width/2;
+                    let tagY=getOffset(tags[i]).top+getOffset(tags[i]).height/2;
+                    console.log(getOffset(tags[i]).left + "+" + getOffset(tags[i]).top);
         
-        tongue.style.backgroundImage.size="100%"
-        tongue.style.transformOrigin="0% 50%"
-        liz.style.backgroundImage.size="100%"
-        liz.style.transformOrigin="0% 50%"
-        liz.style.left="0px"
-        tongue.style.marginLeft="101px"
-        setTimeout(()=>{
-          tongue.style.width=tongueLength+"px"
-          liz.style.left="10px"
-          
-        },500)
-        setTimeout(()=>{
-          tongue.style.marginLeft="0px"
-          tongue.style.width="0px"
-          liz.style.left="-100px"
-          start=false
-        },1000)
-        // tongue.style.width=(tongxueWidth**2+tongueHeight**2)*0.5+"px";
+                    console.log("after calculation "+tagX+tagY);
         
-        // angle between two points:  Math.atan2(p2.y - p1.y, p2.x - p1.x);
-     
+                    let tongueX=0;
+                    // can be changed according to the lizard
+                    let tongueY=window.innerHeight/2;
+                    let a = tagX - tongueX-20;
+                    let b = tagY - tongueY; 
+                    if (tagX==0&&tagY==0) {
+                        // console.log("jumped out");
+                      start=false
+                      return
+                    }
+                    // keep the tongue grow and shrink according to the pos of a,b
+                    let tongueLength = Math.sqrt(a*a + b*b)                    
+                      tongue.style.backgroundImage.size="100%"
+                      tongue.style.transformOrigin="0% 50%"
+                      liz.style.backgroundImage.size="100%"
+                      liz.style.transformOrigin="0% 50%"
+                      liz.style.left="0px"
+                      tongue.style.marginLeft="101px"
+                    
+                      setTimeout(()=>{
+                        tongue.style.width=tongueLength+"px"
+                        liz.style.left="10px"
+                      },500)
+                      setTimeout(()=>{
+                        tongue.style.marginLeft="0px"
+                        tongue.style.width="0px"
+                        liz.style.left="-100px"
+                        start=false
+                      },1000)
+             
+                    let angle = (Math.atan2(b, a)) * 180 / Math.PI;
+                    // tags[i].innerHTML+=angle;
+                    tongue.style.transform="rotate("+angle+"deg)"
+                    chrome.runtime.sendMessage({ type: 'ate a link', content: eatenLink });
 
-    
-      let angle = (Math.atan2(b, a)) * 180 / Math.PI;
-      // tags[i].innerHTML+=angle;
-      tongue.style.transform="rotate("+angle+"deg)"
-      chrome.runtime.sendMessage(
-        { msg: eatenOnes }
-        );
-      // }else{
-      //   start=false
-      // }
-    })
-    
-  }
+                }
+            }
+        })
+
+    }
 }
-  
+
