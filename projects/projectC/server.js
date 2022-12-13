86fe786bb6262b98acaab69bc0e9751a27c90caf
Vivian <xx2185@nyu.edu>
2022-12-13 13:43:50 +0800

    projectConmain page

--- projects/projectC/server.js
+++ projects/projectC/server.js
@@ -1,6 +1,6 @@
-const { log } = require('console');
 const express = require('express');
 const app = express();
+// const port = 3000;
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
